223f2be4bccba6dd5aa5624de0f2ecb1bbeeea85
Prashaath R <fssai.cdc@gmail.com>
2021-02-05 17:38:14 +0530

    Only Zookeeper SSL Working

--- clean_all.sh
+++ clean_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-docker rm -f zoonavigator kafkamanager 
+echo "Removing all containers"
+docker rm -f zoonavigator kafkamanager
+echo "Removing all LxD containers" 
 lxc rm -f node1 node2 node3
+echo "Removing generated keys"
 (cd files/ ; rm -vf *.jks *.cert *.csr *.key *.srl)
+echo "Clean Kafka Clients"
+rm bin
+rm -rf files/kafka_*/
+rm client.properties
--- start_all.sh
+++ start_all.sh
@@ -3,31 +3,27 @@
 # Generate SSL Certs 
 ./gen_ssl.sh
 # Set Up Kubernets Cluster
-sudo ./playbook.yml -t lxd,kafka_install,zk_setup
+sudo ./playbook.yml
 
+sleep 5
 # Check ZooKeeper
 for i in 1 2 3; do nc -vz zookeeper$i 2181; done
 # Check Zookeeper on SSL
 for i in 1 2 3; do nc -vz zookeeper$i 2182; done
-# Start ZooNavigator 
-docker run \
-  -d --network host \
-  -e HTTP_PORT=9001 \
-  -e CONNECTION_LXDZK_NAME="ZooKeeper" \
-  -e CONNECTION_LXDZK_CONN="zookeeper1:2181,zookeeper2:2181,zookeeper3:2181" \
-  --name zoonavigator \
-  --restart unless-stopped \
-  elkozmon/zoonavigator:latest
-
 # Check Kafka
-for i in 1 2 3; do nc -vz kafka$i 9092; done
+#for i in 1 2 3; do nc -vz kafka$i 9092; done
+
+# Start ZooNavigator 
+docker-compose up -d
+
+
 # Start KafkaManager
-docker run \
-  -d --network host \
-  -e  ZOOKEEPER_HOSTS="zookeeper1:2181,zookeeper2:2181,zookeeper3:2181" \
-  --name kafkamanager \
-  --restart unless-stopped \
-  qnib/plain-kafka-manager:latest
+#docker run \
+#  -d --network host \
+#  -e  ZOOKEEPER_HOSTS="zookeeper1:2181,zookeeper2:2181,zookeeper3:2181" \
+#  --name kafkamanager \
+#  --restart unless-stopped \
+#  qnib/plain-kafka-manager:latest
 
 # Unzip kafka for Client
 file=$(ls files/kafka_*.tgz)
